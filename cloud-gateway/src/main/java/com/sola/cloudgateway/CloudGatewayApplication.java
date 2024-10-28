6ee2e386a0e900f46b0f8f409b1ad5cfba4155dd
Olorunsola Daramola <daramolaolorunsola@gmail.com>
2024-10-28 16:49:27 +0000

    Add spring-cloud-gateway update

---
+++ cloud-gateway/src/main/java/com/sola/cloudgateway/CloudGatewayApplication.java
@@ -0,0 +1,10 @@
+package com.sola.cloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class CloudGatewayApplication {
+    public static void main(String [] args ){SpringApplication.run(CloudGatewayApplication.class,args);}
+}
